4def4f2f13c1e0702db6993943ad7bc00248bbd9
benjikpop202 <benjaminsobarzo2018@gmail.com>
2024-02-09 15:53:27 -0300

    actualizaciones de api

--- src/api/API-REST.js
+++ src/api/API-REST.js
@@ -1,1 +1,11 @@
-console.log("hola mundo");
+import express from 'express'
+import {dirname, join} from 'path'
+import { fileURLToPath } from 'url'
+
+const app = express()
+const port = 3000
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+app.listen(port, ()=>{
+    console.log("escuchando en el puerto:",port,"🚀");
+})
