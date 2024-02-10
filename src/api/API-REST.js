8c1c89cbc6bb1c1915449b87394807a405f915c9
benjikpop202 <benjaminsobarzo2018@gmail.com>
2024-02-10 13:54:06 -0300

    cambios 2.0

--- src/api/API-REST.js
+++ src/api/API-REST.js
@@ -1,10 +1,21 @@
-import express from 'express'
-import path, {dirname, join} from 'path'
+const express = require('express')
+const fs = require('fs')
+
 
 const app = express()
 const port = 3000
 
-app.get('/',(req, res)=> res.json({"id":1, "propiedad":"departamento", "habitaciones":2, "baños":1}))
+
+
+app.get('/',(req, res)=> fs.readFile('../Data/datos.json',function(err, data){
+  if(err){
+    res.status(500).send("error en busqueda de propiedades")
+    console.log(err)
+    return
+  }
+  const datos = JSON.parse(data)
+  res.json(datos)
+}))
 
 app.listen(port, ()=>{
     console.log("escuchando en el puerto:",port,"🚀");
