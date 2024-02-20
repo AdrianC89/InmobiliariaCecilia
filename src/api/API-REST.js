e32a1c77ffcce2f0603776299f238d6aaf0c7ccf
benjikpop202 <benjaminsobarzo2018@gmail.com>
2024-02-20 15:49:24 -0300

    añadir archivos a las rutas

--- src/api/API-REST.js
+++ src/api/API-REST.js
@@ -1,22 +1,15 @@
 const express = require('express')
-const fs = require('fs')
+const path = require('path')
 
 
 const app = express()
 const port = 3000
 
+app.use(express.static(path.join(__dirname, '../')));
 
-
-app.get('/',(req, res)=> fs.readFile('src/Data/datos.json',function(err, data){
-  if(err){
-    res.status(500).send("error en busqueda de propiedades")
-    console.log(err)
-    return
-  }
-  const datos = JSON.parse(data)
-  const jsonString = JSON.stringify(datos, null, 4) //para ajustar la vista del json
-  res.send(jsonString)
-}))
+app.get('/',(req, res)=> {
+  res.sendFile(path.join(__dirname, '../pages/index.html'))
+})
 
 app.listen(port, ()=>{
     console.log("escuchando en el puerto:",port,"🚀");
