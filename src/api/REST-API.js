f3d7448ed7e9b155624f4c3eb9c53da068250675
benjikpop202 <benjaminsobarzo2018@gmail.com>
2024-02-20 19:58:25 -0300

    rename api file

---
+++ src/api/REST-API.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const path = require('path')
+
+//ZOTipHOiqCh0u053 contraseña de acceso db
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname, '../')));
+
+//Rutas
+app.get('/', (req, res)=>{
+  res.sendFile(path.join(__dirname, '../pages/','index.html'))
+})
+
+app.get('/:seccion',(req, res)=> {
+  const seccion = req.params.seccion
+  if(seccion == 'propiedades' || seccion == 'tasaciones' || seccion == 'nosotros' || seccion == 'contacto'){
+    res.sendFile(path.join(__dirname, '../pages/',`${seccion}.html`))
+  }else{
+    res.redirect('/')
+  }
+  
+})
+
+
+
+app.listen(port, ()=>{
+    console.log("escuchando en el puerto:",port,"🚀");
+})
