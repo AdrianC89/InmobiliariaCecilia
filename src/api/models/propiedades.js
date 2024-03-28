1ed485694178143e4ceb50716ec30baecfc625bb
Adrián <adriancoroneltandil@gmail.com>
2024-03-27 22:58:41 -0300

    creacion de carrousel y reestructuracion de propiedades

--- src/api/models/propiedades.js
+++ src/api/models/propiedades.js
@@ -10,15 +10,15 @@ const imagenSchema = new Schema({
 
 const propiedadesSchema = new Schema({
     numeroRegistro: { type: String },
-    tipoOperacion: { type: String, enum: ['venta', 'alquiler', 'alquiler temporario', 'venta en otra localidad', 'remate', 'permuta'] },
-    tipoPropiedad: { type: String, enum: ['casa', 'departamento/duplex', 'quinta', 'lote', 'campo', 'galpon', 'local/oficina', 'cabañas/hoteles/otros', 'fondo de comercio', 'cocheras'] },
+    tipoOperacion: { type: String, enum: ['Venta', 'Alquiler', 'Alquiler temporario', 'Venta en otra localidad', 'Remate', 'Permuta'] },
+    tipoPropiedad: { type: String, enum: ['Casa', 'Departamento/Duplex', 'Quinta', 'Lote', 'Campo', 'Galpon', 'Local/Oficina', 'Cabañas/Hoteles/Otros', 'Fondo de comercio', 'Cocheras'] },
     direccion: { type: String },
     direccionBusq: { type: String },
     coordenada1: { type: String },
     coordenada2: { type: String },
     descripcion: { type: String },
     precio: { type: String },
-    moneda: { type: String, enum: ['peso', 'dolar'] },
+    moneda: { type: String, enum: ['Peso', 'Dolar'] },
     dormitorios: { type: String, enum: ['Sin dormitorio', '1', '2', '3', '4', '5', '6', '7', '8'] },
     banos: { type: String, enum: ['Sin baño', '1', '2', '3', '4', '5'] },
     garage: { type: String, enum: ['No tiene garage', '1', '2', '3', '4', '5'] },
